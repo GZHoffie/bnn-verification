cc8db060790ed1a0ad386418c26aed642bee84fc
Gu Zhenhao <guzh@comp.nus.edu.sg>
2024-11-26 11:22:25 +0800

    Fix implementation of GL

--- src/main.py
+++ src/main.py
@@ -52,7 +52,7 @@ class binary_function:
         
         return res
     
-    def sample_paired_input_GL(self, num_samples, J=[]):
+    def goldreich_levin_sample_paired_input(self, num_samples, J=[]):
         """
         Sample a set of paired inputs for the Goldreich-Levin algorithm.
         """
@@ -62,10 +62,11 @@ class binary_function:
             sample_1 = np.random.choice([1, -1], self.n)
             sample_2 = np.random.choice([1, -1], self.n)
             # Sample z uniformly at random on the coordinates in `S`
-            for i in J:
-                z_i = np.random.choice([1, -1])
-                sample_1[i] = z_i
-                sample_2[i] = z_i
+            for i in range(self.n):
+                if i not in J:
+                    z_i = np.random.choice([1, -1])
+                    sample_1[i] = z_i
+                    sample_2[i] = z_i
             
             samples_1.append(sample_1)
             samples_2.append(sample_2)
@@ -75,8 +76,8 @@ class binary_function:
         
         return samples_1, samples_2
     
-    def estimate_weight_GL(self, num_samples, S, J):
-        s1, s2 = self.sample_paired_input_GL(num_samples, J)
+    def goldreich_levin_estimate_weight(self, num_samples, S, J):
+        s1, s2 = self.goldreich_levin_sample_paired_input(num_samples, J)
 
         # Compute function values
         res = self.compute_function_values(s1) * self.compute_chi_S(s1, S) * \
@@ -116,10 +117,7 @@ class binary_function:
         """
         # Compute values of chi_S(x) for all possible inputs.
         assert len(f_values) == len(f_inputs), "The number of inputs and values should be the same."
-        chi_S = np.ones(len(f_values))
-
-        for i in S:
-            chi_S *= f_inputs[:, i]
+        chi_S = self.compute_chi_S(f_inputs, S)
 
         #print(chi_S)
 
@@ -164,60 +162,55 @@ class binary_function:
         return self._compute_single_fourier_coefficient(values, inputs, S)
 
     
-    def GL_bucket(self, num_samples, k, S):
+    def goldreich_levin_bucket(self, num_samples, k, S):
         """
         Compute the Fourier weight of the bucket
         B_k,S = {S cup T: T in {k+1, k+2, ..., n}}.
         """
         # Compute the weight W_S|{k+1,...,n}
-        J = list(range(k, self.n))
-        weight = self.estimate_weight_GL(num_samples, S, J)
+        J = list(range(k))
+        weight = self.goldreich_levin_estimate_weight(num_samples, S, J)
         return weight
 
-    def goldreich_levin_recursive(self, num_samples, k, epsilon, S, estimated_coeffs):
+    def goldreich_levin_recursive(self, num_samples, k, tau, S, estimated_coeffs):
         """
         Recursive procedure to find significant Fourier coefficients.
         """
 
         # Compute the weight of the bucket B_k,S
-        weight = self.GL_bucket(num_samples, k, S)
+        weight = self.goldreich_levin_bucket(num_samples, k, S)
         print(f"Weight of B_{k},{S}: {weight}")
 
-        if abs(weight) < epsilon / 2:
-            return estimated_coeffs
         
         if k == self.n:
             # there is only one set in the bucket
             #print(S)
-            estimated_coeffs[tuple(S)] = self.fourier_approximate_S(num_samples, S)
+            fourier_coeff = self.fourier_approximate_S(num_samples, S)
+            if abs(fourier_coeff) > tau / 2:
+                estimated_coeffs[tuple(S)] = fourier_coeff
+                
+            return estimated_coeffs
+        
+        if weight < tau ** 2 / 2:
             return estimated_coeffs
 
         # Continue if the weight is sufficiently large.
         # Recurse on S union {k}
         S_new = S + [k]
-        estimated_coeffs = self.goldreich_levin_recursive(num_samples, k+1, epsilon, S_new, estimated_coeffs)
+        estimated_coeffs = self.goldreich_levin_recursive(num_samples, k+1, tau, S_new, estimated_coeffs)
 
         # Recurse on S
-        estimated_coeffs = self.goldreich_levin_recursive(num_samples, k+1, epsilon, S, estimated_coeffs)
+        estimated_coeffs = self.goldreich_levin_recursive(num_samples, k+1, tau, S, estimated_coeffs)
         return estimated_coeffs
 
     
-    def goldreich_levin(self, epsilon, num_samples):
+    def goldreich_levin(self, tau, num_samples):
         """
         Implements the Goldreich-Levin algorithm to find significant Fourier coefficients.
-
-        Parameters:
-        - n: Number of input variables.
-        - bnn_function: Function that takes a binary vector x and returns -1 or 1.
-        - epsilon: Threshold for significant Fourier coefficients.
-
-        Returns:
-        - significant_S: List of subsets S where |hat{f}(S)| >= epsilon/2.
-        - estimated_coeffs: Dictionary mapping subsets S to estimated Fourier coefficients.
         """
         estimated_coeffs = {}
         S = []
-        return self.goldreich_levin_recursive(num_samples, 0, epsilon, S, estimated_coeffs)
+        return self.goldreich_levin_recursive(num_samples, 0, tau, S, estimated_coeffs)
 
 
 
@@ -226,7 +219,7 @@ class binary_function:
 if __name__ == "__main__":
     def ltf(x):
         # linear threshold function
-        if x[0] * 1 + x[1] * 1 + x[2] * 1 + x[3] * 1 + x[4] * 1 < 0:
+        if x[0] * 1 + x[1] * 1 + x[2] * -1 + x[3] * 1 + x[4] * 1 < 0:
             return -1
         else:
             return 1
@@ -237,4 +230,4 @@ if __name__ == "__main__":
 
     print(f.fourier_approximate(10000))
 
-    print(f.goldreich_levin(0.01, 1000))
+    print(f.goldreich_levin(0.1, 1000))
